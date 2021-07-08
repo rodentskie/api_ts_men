75f5f3f4450ea660079ece755a068d9e42e45108
rodentskie <rodentskie@gmail.com>
2021-07-08 15:49:05 +0800

    dqwdqw

--- src/data/index.ts
+++ src/data/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const conn = async () => {
     // connect
-    const uri = `mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=false`;
+    const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=false`;
     const db = await connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
